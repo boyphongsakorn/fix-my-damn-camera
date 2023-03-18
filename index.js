778777a8e37599fddcc3e7b3717c3fd50c4c40db
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-03-18 13:34:21 +0700

    ok

--- index.js
+++ index.js
@@ -22,44 +22,44 @@ fastify.listen(3000, (err, address) => {
     fastify.log.info(`server listening on ${address}`);
 });*/
 
-const NodeMediaServer = require('node-media-server');
+// const NodeMediaServer = require('node-media-server');
 
-const config = {
-    logType: 3, // 3 - Log everything (debug)
-    rtmp: {
-        port: 1935,
-        chunk_size: 120000,
-        gop_cache: true,
-        ping: 60,
-        ping_timeout: 120
-    },
-    http: {
-        port: 8000,
-        allow_origin: '*'
-    },
-    relay: {
-        ffmpeg: '/usr/bin/ffmpeg',
-        tasks: [
-            {
-                app: 'cctv',
-                mode: 'static',
-                edge: 'rtsp://192.168.200.1:554',
-                name: 'uterum',
-                rtsp_transport : 'tcp' //['udp', 'tcp', 'udp_multicast', 'http']
-            },
+// const config = {
+//     logType: 3, // 3 - Log everything (debug)
+//     rtmp: {
+//         port: 1935,
+//         chunk_size: 120000,
+//         gop_cache: true,
+//         ping: 60,
+//         ping_timeout: 120
+//     },
+//     http: {
+//         port: 8000,
+//         allow_origin: '*'
+//     },
+//     relay: {
+//         ffmpeg: '/usr/bin/ffmpeg',
+//         tasks: [
 //             {
 //                 app: 'cctv',
 //                 mode: 'static',
-//                 edge: 'rtsp://admin:team1556th@192.168.31.205:554/onvif2',
-//                 name: 'tertwo',
-//                 rtsp_transport : 'udp' //['udp', 'tcp', 'udp_multicast', 'http']
-//             }
-        ]
-    }
-};
+//                 edge: 'rtsp://192.168.200.1:554',
+//                 name: 'uterum',
+//                 rtsp_transport : 'tcp' //['udp', 'tcp', 'udp_multicast', 'http']
+//             },
+// //             {
+// //                 app: 'cctv',
+// //                 mode: 'static',
+// //                 edge: 'rtsp://admin:team1556th@192.168.31.205:554/onvif2',
+// //                 name: 'tertwo',
+// //                 rtsp_transport : 'udp' //['udp', 'tcp', 'udp_multicast', 'http']
+// //             }
+//         ]
+//     }
+// };
 
-var nms = new NodeMediaServer(config)
-nms.run();
+// var nms = new NodeMediaServer(config)
+// nms.run();
 
 /*const streams = [
     {
@@ -75,3 +75,34 @@ nms.run();
     port: 3000, // The port your Express app is listening on
     streams
   });*/
+
+const express = require('express');
+const app = express();
+
+const { proxy, scriptUrl } = require('rtsp-relay')(app);
+
+const handler = proxy({
+  url: `rtsp://192.168.200.1:554`,
+  // if your RTSP stream need credentials, include them in the URL as above
+  verbose: false,
+});
+
+// the endpoint our RTSP uses
+app.ws('/api/stream', handler);
+
+// this is an example html page to view the stream
+app.get('/', (req, res) =>
+  res.send(`
+  <canvas id='canvas'></canvas>
+
+  <script src='${scriptUrl}'></script>
+  <script>
+    loadPlayer({
+      url: 'ws://' + location.host + '/api/stream',
+      canvas: document.getElementById('canvas')
+    });
+  </script>
+`),
+);
+
+app.listen(1935);
